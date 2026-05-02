53dfbe5159724f2efabf199a5990e594ba859f49
Gerson <gersonmachuca24@gmail.com>
2026-05-02 01:44:22 -0500

    feat : agregando EntityGraph para evitar n+1 en usuarios

--- src/main/java/com/sistemabarberia/fadex_backend/auth/usuario/Repository/UsuarioRepository.java
+++ src/main/java/com/sistemabarberia/fadex_backend/auth/usuario/Repository/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package com.sistemabarberia.fadex_backend.auth.usuario.Repository;
 
 import com.sistemabarberia.fadex_backend.auth.usuario.Entity.Usuario;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -23,4 +25,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
      Optional<Usuario> findByUserWithRolesYPermisos(String user);
 
     Optional<Usuario> findByUser(String user);
+    @EntityGraph(attributePaths = "roles")
+    List<Usuario> findAll();
+
 }
