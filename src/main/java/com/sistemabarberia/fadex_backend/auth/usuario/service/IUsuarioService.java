4b2ed27530680f39e0e8c29857c4476f29e6f595
Dejoda <U23216205@utp.edu.pe>
2026-05-15 21:23:45 -0500

    feat: Agregar endpoint para cambio-contraseña

--- src/main/java/com/sistemabarberia/fadex_backend/auth/usuario/service/IUsuarioService.java
+++ src/main/java/com/sistemabarberia/fadex_backend/auth/usuario/service/IUsuarioService.java
@@ -1,9 +1,6 @@
 package com.sistemabarberia.fadex_backend.auth.usuario.service;
 
-import com.sistemabarberia.fadex_backend.auth.usuario.dto.request.CreateBarberoRequest;
-import com.sistemabarberia.fadex_backend.auth.usuario.dto.request.CreateClienteRequest;
-import com.sistemabarberia.fadex_backend.auth.usuario.dto.request.CreateUsuarioRequest;
-import com.sistemabarberia.fadex_backend.auth.usuario.dto.request.RegisterRequest;
+import com.sistemabarberia.fadex_backend.auth.usuario.dto.request.*;
 import com.sistemabarberia.fadex_backend.auth.usuario.dto.response.UsuarioResponse;
 
 import java.util.List;
@@ -16,4 +13,6 @@ public interface IUsuarioService {
     UsuarioResponse crearAdmin(CreateUsuarioRequest request);
     UsuarioResponse crearBarbero(CreateBarberoRequest request);
     UsuarioResponse crearCliente(CreateClienteRequest request);
+    void resetPassword(Integer idUsuario, ResetPasswordRequest request);
+    void updateUsername(Integer id, UpdateUsernameRequest request);
 }
